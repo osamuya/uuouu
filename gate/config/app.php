99f6a2c46eb22a3551c67776dc52f89a262c935e
uuouu <uuouu>
2017-06-11 16:11:45 +0900

    debugbar setting

--- gate/config/app.php
+++ gate/config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -51,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://uuouu.local'),
 
     /*
     |--------------------------------------------------------------------------
@@ -176,6 +176,12 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		 /*
+         * Debugbar
+		 * url: https://github.com/barryvdh/laravel-debugbar
+         */
+		Barryvdh\Debugbar\ServiceProvider::class,
 
     ],
 
@@ -225,7 +231,12 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+		
+		 /*
+         * Debugbar
+		 * url: https://github.com/barryvdh/laravel-debugbar
+         */
+		'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
