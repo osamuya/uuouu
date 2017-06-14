96d1a575f01fb9ae6ba08d6a661cad4f37512f5b
osamuya <oosamuuy@gmail.com>
2017-06-14 21:24:49 +0900

    TOPページのForm

---
+++ gate/resources/views/index.blade.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<!--[if IE 6]> <html class="ie6"> <![endif]-->
+<!--[if IE 7]> <html class="ie7"> <![endif]-->
+<!--[if IE 8]> <html class="ie8"> <![endif]-->
+<!--[if IE 9]> <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width">
+<!--page info-->
+<title></title>
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<!--og-->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="" />
+<meta property="og:image" content="" />
+<meta property="og:title" content="" />
+<meta property="og:description" content="" />
+<meta property="og:site_name" content="" />
+<!--favicon-->
+<link rel="shortcut icon" href=""/>
+<!--css-->
+<link rel="stylesheet" type="text/css" href="/assets/css/index.css" media="all" />
+
+<!--js-->
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="/assets/js/easing.js"></script>
+<script type="text/javascript" src="/assets/js/main.js"></script>
+
+<!--[if lt IE 9]>
+<script type="text/javascript" src="/assets/js/html5shiv-printshiv.js"></script>
+<script type="text/javascript" src="/assets/js/css3-mediaqueries.js"></script>
+<script type="text/javascript" src="/assets/js/flexibility.js"></script>
+<![endif]-->
+
+</head>
+
+<body>
+<header class="header">
+    <h1 class="header__toplogo">uuouu</h1>
+</header>
+<div class="ubody">
+    <div class="signup formarea">
+        <form action="" method="">
+            <div class="formarea__input">
+                <label class="formarea__input__label">User name: </label>
+                <input type="text" name="" id=""  class="formarea__input__field">
+            </div>
+            <div class="formarea__input">
+                <label class="formarea__input__label">Email: </label>
+                <input type="text" name="" id="" class="formarea__input__field">
+            </div>
+            <div class="formarea__input">
+                <label class="formarea__input__label">Password: </label>
+                <input type="text" name="" id="" class="formarea__input__field--small">
+                <input type="submit" name="" id="" class="formarea__input__submit" value="regist">
+            </div>
+        </form>
+    </div>
+    <div class="signin formarea">
+        <form action="" method="">
+            <div class="formarea__input">
+                <label class="formarea__input__label">Email: </label>
+                <input type="text" name="" id="" class="formarea__input__field">
+            </div>
+            <div class="formarea__input">
+                <label class="formarea__input__label">Password: </label>
+                <input type="text" name="" id="" class="formarea__input__field--small">
+                <input type="submit" name="" id="" class="formarea__input__submit" value="Sign in">
+            </div>
+        </form>
+    </div>
+</div>
+<footer class="footer">
+    <p class="footer__copy">Copyright (C) 2017 Saba note. All rights reserved.</p>
+</footer>
+
+
+</body>
+</html>
