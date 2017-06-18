cf44be7b27a71eab2c664c170f1187e618a33299
uuouu <uuouu>
2017-06-18 17:06:53 +0900

    ログイン画面づくり

---
+++ gate/uuo_optimazer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+php composer.phar install
+php composer.phar dump-autoload
+php artisan clear-compiled
+php artisan optimize
+php artisan config:cache
+php artisan route:cache
+php artisan view:clear
