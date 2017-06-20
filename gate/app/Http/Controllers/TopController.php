c8432b5cb3856a69cb92ec3172920509ea8e9bd8
osamuya <oosamuuy@gmail.com>
2017-06-20 19:59:52 +0900

    エラー画面作成

--- gate/app/Http/Controllers/TopController.php
+++ gate/app/Http/Controllers/TopController.php
@@ -1,23 +1,50 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Library\BaseClass;
 
+
+
+/**
+ * TopController 主にサインアップ・サインインなどの認証機能
+ *
+ * [Class詳細]
+ * 出来るだけ細かく書いたほうがよいが、詳細な説明は各メソッドに任せる。
+ * 全体での共通ルールとか仕様を書く。
+ *
+ * @access public
+ * @author omnioo
+ * @copyright omnioo All Rights Reserved
+ * @category Authorization Authorization
+ * @package Laravel5.4
+ */
 class TopController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $hash = BaseClass::makeAccessHash();
-        var_dump($hash);
+//        var_dump($hash);
+        $request->session()->put("regist_status",$hash);
         return view('index')->with('hash',$hash);
     }
 
 
     public function regist(Request $request, $hash)
     {
-//        var_dump($hash);
+
+        if ($request->session()->get("regist_status") == $hash) {
+             //nothing
+        } else {
+            $request->session()->forget("regist_status");
+            // Redirect
+            // Logging
+            abort("500");
+            return false;
+        }
+
+        // save mysql database
+
         return "foo bar";
     }
 }
