da8134e7b03d8527c632d07fb29342c108e7fcf7
uuouu <uuouu>
2017-06-27 07:31:03 +0900

    artisan make:authでやったファイル

--- gate/resources/views/list.blade.php
+++ gate/resources/views/list.blade.php
@@ -2,4 +2,5 @@
 	<li><a href="/" target="_blank">top</a></li>
 	<li><a href="/sample" target="_blank">sample</a></li>
 	<li><a href="/sendmail" target="_blank">sendmail</a></li>
+	<li><a href="/home" target="_blank">home (Auth)</a></li>
 </ul>
